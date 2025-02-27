5743bf29b4bb9a477a415b280c6c258f8655442d
Nícolas <161242131+nicz-gom@users.noreply.github.com>
2025-02-27 00:38:07 -0300

    versão 2.0

--- script.js
+++ script.js
@@ -9,11 +9,16 @@ let pElement = document.createElement("p");
 let pContent = ['No element was passed in the input', 'Your element its from 6 at 9!', 'Your element not its from 6 at 9!'];
 let array = [];
 
-function verify(){
+function receiver(){ //função para receber o parâmetro;
     pElement.innerHTML = '';
     let register = inputElement.value;
 
-    if(register === ''){
+    verify(register); //argumento para o parâmetro;
+}
+
+//função para fazer a verificação do valor que está entrando no input;
+function verify(parameter){ // parâmetro; 
+    if(parameter === ''){
         inputElement.style.border = "1px, solid, red";
         inputElement.style.borderRadius = "3px";
 
@@ -23,7 +28,7 @@ function verify(){
 
         pElement.style.color = "red";
     }else{
-        if(register >= 6 && register <= 9){
+        if(parameter >= 6 && parameter <= 9){
             inputElement.style.border = "1px, solid,rgb(24, 168, 36)";
             inputElement.style.borderRadius = "3px";
 
@@ -33,8 +38,11 @@ function verify(){
 
             pElement.style.color = "rgb(24, 168, 36)";
 
-            //continua... Tem que passar um array vazio!
-            return array[0] = register;
+            //continua... Tem que passar um array vazio!  
+            array[0] = parameter;
+            shuffle(parameter);
+            console.log("Your number is valid!");
+            return console.log(array);     
 
         }else{
             inputElement.style.border = "1px, solid, #FFC222";
@@ -45,14 +53,19 @@ function verify(){
             divElement_Container.appendChild(pElement);
 
             pElement.style.color = "#FFC222";
+
+            array = [];
+            console.log("Cannot used the number!");
+            return console.log(array);
+
         }
     }
 }
 
-if(verify() === ''){
-    console.log(array);
-}else{
-    console.log(array);
+function shuffle(value){
+    alert(value);
+    //tenho que embaralhar em uma lista com o tamanho do argumento que foi passado
 }
 
-addElement.onclick = verify;
+
+addElement.onclick = receiver;
